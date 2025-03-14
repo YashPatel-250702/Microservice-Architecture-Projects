7c850a5222a2370cba6fbb0a65f5c55e9a27fa5f
Yash-123patel <patelyash250702@gmail.com>
2025-03-14 18:44:50 +0530

    Implemented Junit testing for employee service method.

--- Employee-info/src/main/java/com/tekworks/microservice/service/EmployeeService.java
+++ Employee-info/src/main/java/com/tekworks/microservice/service/EmployeeService.java
@@ -4,11 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import com.tekworks.microservice.entity.Employee;
 import com.tekworks.microservice.exception.NoEmployeeFoundException;
---
+++ Employee-info/src/test/java/com/tekworks/microservice/EmployeeServiceTest.java
@@ -0,0 +1,67 @@
+package com.tekworks.microservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.tekworks.microservice.entity.Employee;
+import com.tekworks.microservice.feigncleints.DepartmentFeignCLient;
+import com.tekworks.microservice.model.Department;
+import com.tekworks.microservice.repository.EmployeeRepository;
+import com.tekworks.microservice.service.EmployeeService;
+
+@ExtendWith(MockitoExtension.class)  // Pure unit test without full Spring context
+class EmployeeApplicationTests {
+    
+    @Mock  
+    private EmployeeRepository employeeRepository;
+
+    @Mock
+    private DepartmentFeignCLient departmentFeignCLient;
+
+    @InjectMocks
+    private EmployeeService employeeService;  
+
+    @Test
+    public void testSaveEmployee() {
+        Employee employee = new Employee();
+        employee.setDeptId(1);
+        Department department = new Department();
+        
+        doReturn(department).when(departmentFeignCLient).getDepartmentById(any());
+        doReturn(employee).when(employeeRepository).save(any());
+    
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        assertEquals(employee, savedEmployee);
+    }
+    
+    @Test
+    public void testSaveEmployeeInvalidDepartmentId() {
+        Employee employee = new Employee();
+        employee.setDeptId(1);
+
+        doReturn(null).when(departmentFeignCLient).getDepartmentById(any());
+
+        assertThrows(RuntimeException.class, () -> employeeService.saveEmployee(employee));
+    }
+    
+    @Test
+    public void testSaveEmployeeNullDepartmentId() {
+        Employee employee = new Employee();
+        employee.setDeptId(null);
+
+        assertThrows(IllegalArgumentException.class, () -> employeeService.saveEmployee(employee));
+    }
+    
+    @Test
+    public void testSaveEmployeeNullEmployee() {
+        assertThrows(NullPointerException.class, () -> employeeService.saveEmployee(null));
+    }
+}
