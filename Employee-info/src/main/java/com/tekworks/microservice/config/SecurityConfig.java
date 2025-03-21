3e3e31bf2f63ce44b02382aea73a8456d02f5a8d
Yash-123patel <patelyash250702@gmail.com>
2025-03-21 18:35:21 +0530

    added configuration in employee service for okta security

---
+++ Employee-info/src/main/java/com/tekworks/microservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.tekworks.microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.oauth2ResourceServer()
+		.jwt();
+		return http.build();
+	}
+}
---
+++ Employee-info/src/main/java/com/tekworks/microservice/config/interceptor/FiegnClientInterseptor.java
@@ -0,0 +1,14 @@
+package com.tekworks.microservice.config.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FiegnClientInterseptor implements RequestInterceptor{
+
+	@Override
+	public void apply(RequestTemplate template) {
+		
+		
+	}
+
+}
